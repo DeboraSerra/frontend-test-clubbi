e16aa7c3742cc19fbb5f54df6beeedb229b37f76
Debora Serra <debora.r.serra@gmail.com>
2022-10-08 10:20:16 -0300

    feat: Create pages FilmId and MyFilms

---
+++ src/pages/FilmId.js
@@ -0,0 +1,13 @@
+import BigCard from "../components/Cards/BigCard";
+import Header from "../components/Header/Header";
+
+function FilmId() {
+  return (
+    <div>
+      <Header />
+      <BigCard />
+    </div>
+  )
+}
+
+export default FilmId;
---
+++ src/pages/MyFilms.js
@@ -0,0 +1,9 @@
+function MyFilms() {
+  return (
+    <div>
+      <h1>MyFilms</h1>
+    </div>
+  )
+}
+
+export default MyFilms;
